165e38411fff504406e5fb663152d9ef0d3c8d7d
Luiz Rafael <luizrafael404101@gmail.com>
2026-09-06 15:33:32 -0300

    fix: corrige tabuada para iniciar em 1 e adiciona objetivo

--- exercicio-2.js
+++ exercicio-2.js
@@ -1,7 +1,9 @@
+//OBJETIVO:
+// Imprimir a tabuada de multiplicação dos números de 1 a 10, exibindo para cada número todas as suas multiplicações de 1 a 10.
 for (let i = 1; i <= 10; i++) {
     console.log(`\nTabuada do número ${i}`)
 
-    for (let j = 0; j <=10; j++) {
+    for (let j = 1; j <=10; j++) {
         console.log(`${i} X ${j} = ${i*j}`)
         
     }
