024cb6d0b4181ccfb2f3c6182eca19b854897db6
Luiz Rafael <luizrafael404101@gmail.com>
2026-09-06 15:35:36 -0300

    adicionado comentário de objetivo

--- exercicio-3.js
+++ exercicio-3.js
@@ -1,5 +1,7 @@
+//OBJETIVO:
+// Fazer função que retorne o valor aproximado de km em milhas 
 function conversor(km) {
     console.log(`Convertendo ${km}Km em milha(s)...`)
     return km/1.609;
-};
+}
 console.log(`O resultado é ${conversor(10).toFixed(2)} milha(s)`);
